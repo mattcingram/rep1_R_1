a8dd275b42cc3f241e97a4ba37ff8a6ee9006bac
Matt Ingram <40709229+mattcingram@users.noreply.github.com>
2021-02-11 16:29:44 -0500

    Add files via upload

---
+++ install.R
@@ -0,0 +1,5 @@
+# packages from original publication
+mypackages <- c("Hmisc", "tidyverse", "GGally", "car", "interplot", "interflex", "gridExtra", "coefplot", "psych")
+install.packages(mypackages, verbose=TRUE)
+# additional packages
+install.packages(c("pacman", "stargazer", "data.table", "xtable"))
